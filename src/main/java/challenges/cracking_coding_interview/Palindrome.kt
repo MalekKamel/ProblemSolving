08417099a885b6d77daa89a2b9c3cded2ce260e4
Shaban <sh3ban.kamel@gmail.com>
2021-04-21 14:55:43 +0200

    Add Palindrome

---
+++ src/main/java/challenges/cracking_coding_interview/Palindrome.kt
@@ -0,0 +1,37 @@
+package challenges.cracking_coding_interview
+
+import java.util.*
+
+object Palindrome {
+
+    private fun solve(s: String): Boolean {
+        val string = s.replace(" ", "")
+        val chars = string.toCharArray()
+
+        val stack = Stack<Char>()
+        for (char in chars) {
+            stack.push(char)
+        }
+
+        for (char in chars) {
+            val char2 = stack.pop()
+            if (char != char2) return false
+        }
+        return true
+    }
+
+    private fun solve2(s: String): String {
+        val string = s.replace("\\s".toRegex(), "")
+        val isPalindrome = string == StringBuilder(string).reverse().toString()
+        return isPalindrome.toString()
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(solve("taco cat")) // true
+        println(solve2("taco cat")) // true
+        println(solve("atco cta")) // true
+        println(solve("eye")) // true
+        println(solve("taco cate")) // false
+    }
+}
