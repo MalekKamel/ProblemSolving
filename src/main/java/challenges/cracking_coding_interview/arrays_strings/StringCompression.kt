a8d58bf119f6f984ce5fc65d35662adea3e4bc3d
Shaban <sh3ban.kamel@gmail.com>
2021-10-11 08:56:42 +0200

    Add StringCompression

---
+++ src/main/java/challenges/cracking_coding_interview/arrays_strings/StringCompression.kt
@@ -0,0 +1,80 @@
+package challenges.cracking_coding_interview.arrays_strings
+
+/*
+Implement a method to perform basic string compression using
+the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3.
+If the "compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ */
+
+object StringCompression {
+    fun compress(str: String): String {
+        val compressed = StringBuilder()
+        var countConsecutive = 0
+        for (i in str.indices) {
+            countConsecutive++
+
+            /* If next character is different than current, append this char to result.*/
+            if (i + 1 >= str.length || str[i] != str[i + 1]) {
+                compressed.append(str[i])
+                compressed.append(countConsecutive)
+                countConsecutive = 0
+            }
+        }
+        return if (compressed.length < str.length) compressed.toString() else str
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val str = "aa"
+        println(str)
+        println(compress(str))
+    }
+}
+
+/**
+ * Instead,we can check in advance.This will be more optimal in cases where we don't have a large number of
+ * repeating characters.It will avoid us having to create a string that we never use.
+ * The downside of this is that it causes a second loop through the characters and also adds nearly duplicated code.
+ */
+object StringCompression2 {
+    private fun compress(str: String): String {
+        val finalLength = countCompression(str)
+        if (finalLength >= str.length) return str
+        val compressed = StringBuffer(finalLength) // initialize capacity
+        var countConsecutive = 0
+        for (i in str.indices) {
+            countConsecutive++
+
+            /* If next character is different than current, append this char to result.*/
+            if (i + 1 >= str.length || str[i] != str[i + 1]) {
+                compressed.append(str[i])
+                compressed.append(countConsecutive)
+                countConsecutive = 0
+            }
+        }
+        return compressed.toString()
+    }
+
+    private fun countCompression(str: String): Int {
+        var compressedLength = 0
+        var countConsecutive = 0
+        for (i in str.indices) {
+            countConsecutive++
+
+            /* If next character is different than current, append this char to result.*/
+            if (i + 1 >= str.length || str[i] != str[i + 1]) {
+                compressedLength += 1 + countConsecutive.toString().length
+                countConsecutive = 0
+            }
+        }
+        return compressedLength
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val str = "aa"
+        println(str)
+        println(compress(str))
+    }
+}
