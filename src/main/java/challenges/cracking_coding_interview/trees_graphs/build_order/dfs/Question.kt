421d94cbd5dec928598d88ca4dcfdbd3c54edc19
Shaban <sh3ban.kamel@gmail.com>
2021-11-20 13:56:35 +0200

    Improve code

--- src/main/java/challenges/cracking_coding_interview/trees_graphs/build_order/dfs/Question.kt
+++ src/main/java/challenges/cracking_coding_interview/trees_graphs/build_order/dfs/Question.kt
@@ -2,6 +2,17 @@ package challenges.cracking_coding_interview.trees_graphs.build_order.dfs
 
 import java.util.*
 
+/**
+ * You are given a list of projects and a list of dependencies (which is a list of pairs of projects,
+ * where the second project is dependent on the first project).
+ * All of a project's dependencies must be built before the project is.
+ * Find a build order that will allow the projects to be built.
+ * If there is no valid build order, return an error.
+ * EXAMPLE
+ * Input:
+ * Projects: a, b, c, d, e, f
+ * Dependencies: (a, d), (f, b), (b, d), (f, a), (d, c) Output: f, e, a, b, d, c
+ */
 object Question {
 
     private fun buildOrderWrapper(
