b96f93c9656c67ba09a1e1f08a98ea60c9281f9f
Shaban Kamel <sha@Shabans-MacBook-Pro.local>
2022-03-07 17:48:47 +0200

    Improve code

--- src/main/java/challenges/leetcode/PalindromeNumber.kt
+++ src/main/java/challenges/leetcode/PalindromeNumber.kt
@@ -81,10 +81,12 @@ object PalindromeNumber {
         var remainder: Int
         val original = num
         while (num != 0) {
-            remainder = num % 10 // reversed integer is stored in variable
-            reversed =
-                reversed * 10 + remainder //multiply reversed by 10 then add the remainder, so it gets stored at next decimal place.
-            num /= 10 //the last digit is removed from num after division by 10.
+            // reversed integer is stored in variable
+            remainder = num % 10
+            // multiply reversed by 10 then add the remainder, so it gets stored at next decimal place.
+            reversed = reversed * 10 + remainder
+            // the last digit is removed from num after division by 10.
+            num /= 10
         }
         // palindrome if original and reversed are equal
         return original == reversed
