0f4bd40a3fb236d4d873a60ab1e6b6cf45956d1e
Shaban <sh3ban.kamel@gmail.com>
2022-09-14 23:38:35 +0200

    Add MinSizeSubArraySum

---
+++ src/main/java/challenges/grokking_coding_interview/sliding_window/MinSizeSubArraySum.kt
@@ -0,0 +1,29 @@
+package challenges.grokking_coding_interview.sliding_window
+
+internal object MinSizeSubArraySum {
+    private fun findMinSubArray(S: Int, a: IntArray): Int {
+        val n = a.size
+        var lengthOfSmallestSubarray = Int.MAX_VALUE
+        var windowSum = 0
+        var windowStart = 0
+        for (windowEnd in 0 until n) {
+            windowSum += a[windowEnd] // Add the next element to the window
+            while (windowSum >= S) { // Shrink the window as small as possible until the 'windowSum' is smaller than 'K'
+                lengthOfSmallestSubarray = Math.min(lengthOfSmallestSubarray, windowEnd - windowStart + 1)
+                windowSum -= a[windowStart] // Discard the element at 'windowStart' since it is going out of the window
+                windowStart++ // Shrink the window
+            }
+        }
+        return if (lengthOfSmallestSubarray == Int.MAX_VALUE) 0 else lengthOfSmallestSubarray
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var result = findMinSubArray(7, intArrayOf(2, 1, 5, 2, 3, 2))
+        println("Smallest subarray length: $result")
+        result = findMinSubArray(8, intArrayOf(3, 4, 1, 1, 6))
+        println("Smallest subarray length: $result")
+        result = findMinSubArray(8, intArrayOf(2, 1, 5, 2, 3, 2))
+        println("Smallest subarray length: $result")
+    }
+}
