4f50b1875994d02d51748f84900485ce322687b0
Shaban <sh3ban.kamel@gmail.com>
2022-09-16 10:42:48 +0200

    Add problem description of MinSizeSubArraySum

--- src/main/java/challenges/grokking_coding_interview/sliding_window/MinSizeSubArraySum.kt
+++ src/main/java/challenges/grokking_coding_interview/sliding_window/MinSizeSubArraySum.kt
@@ -1,5 +1,12 @@
 package challenges.grokking_coding_interview.sliding_window
 
+/**
+ * Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous
+ * subarray whose sum is greater than or equal to ‘S’.
+ * Return 0 if no such subarray exists.
+ *
+ * https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
+ */
 internal object MinSizeSubArraySum {
     private fun findMinSubArray(S: Int, a: IntArray): Int {
         val n = a.size
