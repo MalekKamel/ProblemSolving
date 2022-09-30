574014c0bfb278ce86a7915d3bd6f4baeaab163a
Shaban <sh3ban.kamel@gmail.com>
2022-09-30 03:15:51 +0200

    Add description of StringPermutation & StringAnagrams

--- src/main/java/challenges/grokking_coding_interview/sliding_window/_10/StringAnagrams.kt
+++ src/main/java/challenges/grokking_coding_interview/sliding_window/_10/StringAnagrams.kt
@@ -1,5 +1,21 @@
 package challenges.grokking_coding_interview.sliding_window._10
 
+/**
+ * Given a string and a pattern, find all anagrams of the pattern in the given string.
+ * Every anagram is a permutation of a string. As we know, when we are not allowed to repeat characters
+ * while finding permutations of a string, we get N! permutations (or anagrams) of a string having N
+ * characters. For example, here are the six anagrams of the string “abc”:
+ * abc
+ * acb
+ * bac
+ * bca
+ * cab
+ * cba
+ *
+ * Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
+ *
+ * https://www.educative.io/courses/grokking-the-coding-interview/xl2g3vxrMq3
+ */
 object StringAnagrams {
 
     private fun findStringAnagrams(str: String, pattern: String): List<Int> {
--- src/main/java/challenges/grokking_coding_interview/sliding_window/_9/StringPermutation.kt
+++ src/main/java/challenges/grokking_coding_interview/sliding_window/_9/StringPermutation.kt
@@ -1,6 +1,18 @@
 package challenges.grokking_coding_interview.sliding_window._9
 
-
+/**
+ * Given a string and a pattern, find out if the string contains any permutation of the pattern.
+ * Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
+ * abc
+ * acb
+ * bac
+ * bca
+ * cab
+ * cba
+ * If a string has ‘n’ distinct characters, it will have n! permutations.
+ *
+ * https://www.educative.io/courses/grokking-the-coding-interview/N0o9QnPLKNv
+ */
 object StringPermutation {
     private fun findPermutation(str: String, pattern: String): Boolean {
         var windowStart = 0
