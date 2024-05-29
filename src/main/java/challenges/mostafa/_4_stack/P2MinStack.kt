dcf4a04d779d89a826fe531ca79a94e6fb504843
Shaban Kamel <sha.kamel.eng@gmail.com>
2024-05-29 16:03:41 +0300

    Add problems to mostafa

---
+++ src/main/java/challenges/mostafa/_4_stack/P2MinStack.kt
@@ -0,0 +1,100 @@
+package challenges.mostafa._4_stack
+
+/**
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+https://leetcode.com/problems/min-stack/description/
+ */
+
+internal object P2MinStack {
+
+    class MinStack {
+        private val stack: MutableList<Pair<Int, Int>> = mutableListOf()
+
+        fun push(value: Int) {
+            // If the stack is empty, push the value and the value as the minimum
+            if (stack.isEmpty()) {
+                stack.add(Pair(value, value))
+            } else {
+                // Otherwise, push the value and the new minimum value
+                val currentMin = stack.last().second
+                stack.add(Pair(value, minOf(currentMin, value)))
+            }
+        }
+
+        fun pop() {
+            stack.removeAt(stack.lastIndex)
+        }
+
+        fun top(): Int {
+            return stack.last().first
+        }
+
+        fun getMin(): Int {
+            return stack.last().second
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val minStack = MinStack()
+
+        minStack.push(5)
+        println("Pushed 5 to the stack. Current minimum: ${minStack.getMin()}")
+        // Output: Pushed 5 to the stack. Current minimum: 5
+
+        minStack.push(2)
+        println("Pushed 2 to the stack. Current minimum: ${minStack.getMin()}")
+        // Output: Pushed 2 to the stack. Current minimum: 2
+
+        minStack.push(3)
+        println("Pushed 3 to the stack. Current minimum: ${minStack.getMin()}")
+        // Output: Pushed 3 to the stack. Current minimum: 2
+
+        println("Top element: ${minStack.top()}")
+        // Output: Top element: 3
+
+        minStack.pop()
+        println("Popped the top element. Current minimum: ${minStack.getMin()}")
+        // Output: Popped the top element. Current minimum: 2
+
+        println("Top element: ${minStack.top()}")
+        // Output: Top element: 2
+    }
+}
---
+++ src/main/java/challenges/mostafa/_4_stack/P3DailyTemperatures.kt
@@ -0,0 +1,57 @@
+package challenges.mostafa._4_stack
+
+import java.util.Stack
+
+/**
+Given an array of integers temperatures represents the daily temperatures, return an array
+answer such that answer[i] is the number of days you have to wait after the ith day to get
+a warmer temperature. If there is no future day for which this is possible,
+keep answer[i] == 0 instead.
+
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+
+https://leetcode.com/problems/daily-temperatures/description/
+ */
+
+internal object P3DailyTemperatures {
+
+    private fun dailyTemperatures(temperatures: IntArray): IntArray {
+        val n = temperatures.size
+        val answer = IntArray(n)
+        val stack = Stack<Int>()
+
+        for (i in 0 until n) {
+            while (stack.isNotEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                val prevIndex = stack.pop()
+                answer[prevIndex] = i - prevIndex
+            }
+            stack.push(i)
+        }
+
+        return answer
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val temperatures = intArrayOf(73, 74, 75, 71, 69, 72, 76, 73)
+        val answer = dailyTemperatures(temperatures)
+        println(answer.contentToString()) // Output: [1, 1, 4, 2, 1, 1, 0, 0]
+    }
+}
