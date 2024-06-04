e0ca0a143b0c89767cae44b0a19e3e7fac6569ff
Shaban Kamel <sha.kamel.eng@gmail.com>
2024-06-05 01:06:12 +0300

    Add problems to mostafa

---
+++ src/main/java/challenges/mostafa/_4_stack/P11LongestValidParentheses.kt
@@ -0,0 +1,79 @@
+package challenges.mostafa._4_stack
+
+import java.util.Stack
+
+/**
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
+substring
+.
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+Constraints:
+
+0 <= s.length <= 3 * 10^4
+s[i] is '(', or ')'.
+
+https://leetcode.com/problems/longest-valid-parentheses/description/
+ */
+
+internal object P11LongestValidParentheses {
+
+    private fun longestValidParentheses(s: String): Int {
+        if (s.isEmpty()) return 0
+
+        val stack = Stack<Int>()
+        stack.push(-1)
+        var maxLength = 0
+
+        for (i in s.indices) {
+            when (s[i]) {
+                '(' -> {
+                    stack.push(i)
+                }
+
+                ')' -> {
+                    stack.pop()
+                    // If the stack is now empty, it means the current closing parenthesis doesn't have
+                    // a matching opening parenthesis. In this case, we push the current index
+                    // onto the stack to ensure that the next opening parenthesis will be correctly matched.
+                    if (stack.isEmpty()) {
+                        stack.push(i)
+                        continue
+                    }
+                    maxLength = maxOf(maxLength, i - stack.last())
+                }
+            }
+        }
+
+        return maxLength
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val examples = listOf(
+            "(()" to 2,
+            ")()())" to 4,
+            "" to 0
+        )
+
+        for ((input, expected) in examples) {
+            val result = longestValidParentheses(input)
+            println("Input: $input, Output: $result, Expected: $expected")
+            check(result == expected) { "Test case failed for input: $input" }
+        }
+    }
+}
